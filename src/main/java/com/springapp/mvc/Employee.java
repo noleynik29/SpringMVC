42511036a0717bb6ff2804c5c68d0a450e4a21e1
noleynik29 <noleynik29@gmail.com>
2022-05-23 13:19:20 +0300

    Java Spring MVC App using: spring context, spring core, spring beans, spring web mvc, javax servlet and hibernate.

---
+++ src/main/java/com/springapp/mvc/Employee.java
@@ -0,0 +1,143 @@
+package com.springapp.mvc;
+
+import com.springapp.mvc.validation.CheckEmail;
+
+import javax.validation.constraints.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Employee {
+    @Size(min = 2, message = "Name must be min 2 symbols")
+    private String name;
+    @NotBlank(message = "Surname is required field")
+    private String surname;
+    @Min(value = 700, message = "Must be greater than 699")
+    @Max(value = 1250, message = "Must be less than 1251")
+    private int salary;
+    private String department;
+    private Map<String, String> departments;
+    private String carBrand;
+    private Map<String, String> carBrands;
+    private String[] languages;
+    private Map<String, String> languagesMap;
+    @Pattern(regexp = "\\d{3}-\\d{2}-\\d{2}", message = "Please use pattern XXX-XX-XX")
+    private String phoneNumber;
+    @CheckEmail
+    private String email;
+
+    public Employee() {
+        departments = new HashMap<>();
+        departments.put("Information Technology", "IT");
+        departments.put("Human Resources", "HR");
+        departments.put("Sales", "Sales");
+
+        carBrands = new HashMap<>();
+        carBrands.put("Audi", "Audi");
+        carBrands.put("Cadillac", "Cadillac");
+        carBrands.put("Chevrolet", "Chevrolet");
+        carBrands.put("Dodge", "Dodge");
+
+        languagesMap = new HashMap<>();
+        languagesMap.put("English", "EN");
+        languagesMap.put("Deutch", "DE");
+        languagesMap.put("French", "FR");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Map<String, String> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(Map<String, String> departments) {
+        this.departments = departments;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public Map<String, String> getCarBrands() {
+        return carBrands;
+    }
+
+    public void setCarBrands(Map<String, String> carBrands) {
+        this.carBrands = carBrands;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
+    }
+
+    public Map<String, String> getLanguagesMap() {
+        return languagesMap;
+    }
+
+    public void setLanguagesMap(Map<String, String> languagesMap) {
+        this.languagesMap = languagesMap;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
---
+++ target/com.springapp.mvc/WEB-INF/view/first-view.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2>Hello</h2>
+<br>
+<br>
+
+<a href="askDetails">Please write your details</a>
+</body>
+
+</html>
